2a3c7972e21d54f3c0467e210500d1e0e9f007e5
POLLY1515 <amarantepolly2030@gmail.com>
2024-07-19 09:31:00 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/DSCatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/DSCatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.DSCatalog.entities.Category;
 import com.devsuperior.DSCatalog.repositories.CategoryRepository;
@@ -14,6 +15,7 @@ public class CategoryService {
 	@Autowired
 	CategoryRepository repository;
 
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		return repository.findAll();
 	}
