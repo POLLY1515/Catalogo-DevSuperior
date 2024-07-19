07a7b0c627304348e77d4a5eb9c9fa79475fb91d
POLLY1515 <amarantepolly2030@gmail.com>
2024-07-19 10:47:35 -0300

    Buscando Categoria por id

--- backend/src/main/java/com/devsuperior/DSCatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/DSCatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.DSCatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,17 @@ public class CategoryService {
 		List<Category> list = repository.findAll();
 		//Converter a lista de category para uma lista de categoryDTO
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		//Optional evita trabalhar com o valor nulo
+		Optional<Category> obj = repository.findById(id);
+		//O metodo get do optional,obtem o objeto que esta dentro do optional
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
+		
 		
 	}
 }
